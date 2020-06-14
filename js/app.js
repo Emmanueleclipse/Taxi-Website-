8e5fa74cd0d8f2ed23fe26f893069f7afa599f4b
Emmannuel Sunday <sunasunday111@gmail.com>
2020-06-14 07:54:02 +0300

    intial commit (any name )

---
+++ js/app.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#datepicker").datepicker({
+        dateFormat: "dd/mm/yy",
+        onSelect: function (date, picker) {
+            funcCall(date);  //call you function here
+        }
+    });
+})
+function funcCall(date) {
+    //alert('your function called with date= ' + date);
+    console.log(date)
+}
